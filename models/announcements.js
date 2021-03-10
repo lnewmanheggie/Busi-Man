0732a88c433c0684ea1578546c493617a9f038c5
lnewmanheggie <lnewmanheggie@gmail.com>
2021-03-10 12:20:07 -0600

    user Schema

--- models/announcements.js
+++ models/announcements.js
@@ -11,29 +11,3 @@ const AnnoucementsSchema = new Schema({
 const Annoucements = mongoose.model("Item", AnnoucementsSchema);
 
 module.exports = Annoucements;
-
-// const mongoose = require("mongoose");
-// const db = require("../models");
-// mongoose.connect(
-//     process.env.MONGODB_URI ||
-//     "mongodb://localhost/busiman"
-// );
-// const AnnoucementsSeed = [
-//     {
-//     nameofemployee: "",
-//     date: "date here?",
-//     body: "tom has added apples to the store."
-  
-//     }
-// ];
-// db.Annoucements
-//   .remove({})
-//   .then(() => db.Annoucements.collection.insertMany(AnnoucementsSeed))
-//   .then(data => {
-//     console.log(data.result.n + " records inserted!");
-//     process.exit(0);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//     process.exit(1);
-//   });
