aa046d7324bdbab49d97f7cf805538980bf36ec1
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-03-10 12:01:03 -0600

    Inventory schema and seed file

---
+++ models/inventory.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const inventorySchema = new Schema({
+  barcode: { type: Number, required: true },
+  name: { type: String, required: true },
+  count: { type: Number, required: true },
+  price: { type: Number, required: true}
+});
+
+const Inventory = mongoose.model("Item", inventorySchema);
+
+module.exports = Inventory;
