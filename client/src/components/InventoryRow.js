563fd425f260b922ff62c33e8d5af6290d3af3c2
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-03-11 10:59:10 -0600

    Setup InventoryTable component

---
+++ client/src/components/InventoryRow.js
@@ -0,0 +1,39 @@
+import React from 'react';
+​
+function InventoryRow({ first, last, dob, email, thumbnail, cell }) {
+​
+    const styles = {
+        cell: {
+            backgroundColor: "lightgrey"
+        },
+​
+        email: {
+            backgroundColor: "lightgrey",
+            fontSize: "0.9rem"
+        }
+    }
+​
+    return (
+        <>
+            <div className="cell" style={styles.cell}>
+                <figure className="image is-64x64">
+                    <img src={thumbnail} />
+                </figure>
+            </div>
+            <div className="cell" style={styles.cell}>
+                <div>{first} {last}</div>
+            </div>
+            <div className="cell" style={styles.email}>
+                <div>{email}</div>
+            </div>
+            <div className="cell" style={styles.cell}>
+                <div>{cell}</div>
+            </div>
+            <div className="cell" style={styles.cell}>
+                <div>{dob}</div>
+            </div>
+        </>
+    )
+}
+​
+export default EmployeeRow;
---
+++ client/src/components/InventoryTable.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import InventoryRow from '';
+
+function InventoryTable(props) {
+    return (
+
+            <>
+                {
+                    props.inventory.map((inv, i) => {
+                        return (
+                            <InventoryRow 
+                                key={i} 
+                                code={inv.barcode}
+                                name={inv.name}
+                                count={inv.count}
+                                price={inv.price}
+                            />
+                        )
+                    })
+                }
+            </>
+    );
+}
+
+
+
+export default InventoryTable;
