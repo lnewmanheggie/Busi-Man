f6244d53b1d8978e6d6cc594e70452bc874cb7dc
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-03-11 10:34:36 -0600

    Intial Inventory page

--- client/src/pages/Inventory.js
+++ client/src/pages/Inventory.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Link, useLocation } from 'react-router-dom';
 
 function Inventory() {
+    const styles = {
+
+        container: {
+            backgroundColor: "white",
+        },
+
+        link: {
+            textDecoration: 'none'
+        }
+    }
+
+    const location = useLocation();
+
+
     return(
         <div>
-            <h1>View Inventory PAGE</h1>
-            
+            <Navbar location={location.pathname}/>
+            <Header heading={'Inventory'}/>
+            <Footer />
         </div>
     )
 }
