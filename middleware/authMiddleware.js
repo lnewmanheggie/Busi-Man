1e6894cd5d9a70f68ad5343e4a5835cdad1daa1b
lnewmanheggie <lnewmanheggie@gmail.com>
2021-03-15 12:40:23 -0500

    get rid of cookie

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,7 +5,10 @@ const User = require('./../models/users');
 exports.checkUser = catchAsync(async(req, res, next) => {
     let currentUser;
     console.log(req.headers)
-    let token = req.headers?.cookie?.split(' ')[1].startsWith('jwt') || req.headers.customheader.split(' ')[1]
+    let token = req.headers.customheader.split(' ')[1]
+    console.log(token);
+
+    // req.headers?.cookie?.split(' ')[1].startsWith('jwt') || 
     
     if (token) {
         // const token = req.headers.cookie.split(' ')[1].substring(4);
