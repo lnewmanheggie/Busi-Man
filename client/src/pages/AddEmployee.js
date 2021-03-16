99d8a7fb545b1c160183082681e506fdbb959602
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-03-16 12:39:07 -0500

    Password generator works

--- client/src/pages/AddEmployee.js
+++ client/src/pages/AddEmployee.js
@@ -3,11 +3,12 @@ import useAuth from '../utils/useAuth';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
-// import EmployeeApi from '../utils/EmployeeApi';
 // import { Link, useLocation } from 'react-router-dom';
 import { Input, TextArea,} from "../components/Form";
 import Button from "../components/Button";
 import UserApi from "../utils/UserApi";
+
+
 
 function AddEmployee() {
     const [values, setValues] = useState({
@@ -18,41 +19,6 @@ function AddEmployee() {
       manager: false,
       password: ''
   })
-    // const [formObject, setFormObject] = useState({})
-
-    // useEffect(() => {
-    //     loadEmployee()
-    //   }, [])
-
-      // function loadEmployee() {
-      //   EmployeeApi.getEmployees()
-      //     .then(res => 
-      //       setEmployee(res.data)
-      //     )
-      //     .catch(err => console.log(err));
-      // };
-
-      // function deleteEmployee(id) {
-      //   EmployeeApi.deleteEmployee(id)
-      //     .then(res => loadEmployee())
-      //     .catch(err => console.log(err));
-      // }
-
-    // function handleFormSubmit(event) {
-    //     event.preventDefault();
-    //     if (formObject.first_name && formObject.last_name && formObject.email && formObject.isManager) {
-    //         EmployeeApi.saveEmployee({
-    //         first_name: formObject.first_name,
-    //         last_name: formObject.last_name,
-    //         email: formObject.email,
-    //         // company: formObject.company,
-    //         isManager: formObject.isManager
-
-    //       })
-    //         .then(res => loadEmployee())
-    //         .catch(err => console.log(err));
-    //     }
-    //   };
 
     // Handles updating component state when the user types into the input field
     function handleInputChange(event) {
@@ -67,18 +33,22 @@ function AddEmployee() {
       try {
         let response = await UserApi.getUsers()
         const userCompany = response.data.currentUser.company;
-        // const passwordCharacters =[]
-        // let charCodes = LOWERCASE_CHAR_CODES
-        // for (let i= 0; i < characterAmount; i++) {
-        //   const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
-        //   passwordCharacters.push(String.fromCharCode(characterCode))}
-        
-      } catch (error) {
+        console.log(randomPassword())
+        } catch (error) {
         console.log(error);
       }
     }
 
-    // const location = useLocation();
+    function randomPassword(){
+      const charString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz1234567890!@#$%&*";
+    let passGenerated = "";
+    for (let i = 0; i < 20; i++) {
+    passGenerated += charString.charAt(Math.floor(Math.random() * charString.length)); 
+    } 
+    passGenerated += "CHANGEME"
+    setValues({...values, password: passGenerated})
+  } 
+  
 
     return(
         <div>
