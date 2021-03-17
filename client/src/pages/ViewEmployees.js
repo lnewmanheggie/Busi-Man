9bc756887f47045529e7b1396545abf988711d1b
Gustaf Gonzalez <tony.campoy@gmail.com>
2021-03-17 11:47:05 -0500

    Setup View Employee Page

--- client/src/pages/ViewEmployees.js
+++ client/src/pages/ViewEmployees.js
@@ -1,9 +1,77 @@
 import React, {useState, useEffect} from 'react';
 import useAuth from '../utils/useAuth';
 import EmployeeApi from '../utils/EmployeeApi';
+import Navbar from '../components/Navbar';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+
+function SearchBar(props) {
+    return (
+            <form>
+                <div className="control">
+                    <input
+                        className="input"
+                        type="text"
+                        placeholder="Search"
+                        onChange={props.handleInputChange}
+                        value={props.search}
+                        handleSubmit ={props.handleFilter}
+                        name="search"
+                    />
+                </div>
+            </form>
+    )
+}
 
 
 function ViewEmployees() {
+    const styles = {
+
+        cell: {
+            fontWeight: 'bold'
+        },
+
+        container: {
+            backgroundColor: "white",
+        },
+
+        link: {
+            textDecoration: 'none'
+        }
+    };
+
+    const [search, setSearch] = useState();
+    const [filteredEmployees, setFiltered] =useState();
+    const [employees, setEmployees] = useState([])
+
+    useEffect(() => {
+        loadEmployees();
+  
+      }, [])
+
+    function loadEmployees() {
+        EmployeeApi.getEmployees()
+        .then(res => {
+          setEmployees(res.data)
+          setFiltered(res.data)
+        })
+        .catch(err => console.log(err));
+    };
+
+    const handleInputChange = event => { 
+        setSearch(event.target.value);
+       
+        const employeeTempArray = filteredEmployees.filter(item =>{
+            return item.name.toLowerCase().includes(search?.toLowerCase());
+         
+        })
+        
+        setFiltered(employeeTempArray)
+        if (search === "" ){
+            setFiltered (employees)
+        }
+        console.log(employeeTempArray);
+    };
 
     useAuth();
 
@@ -15,8 +83,33 @@ function ViewEmployees() {
 
     return(
         <div>
-            <h1>View employees PAGE</h1>
+            <Navbar/>
+            <Header heading={'View Employees'}/>
+            <SearchBar value= {search} handleInputChange = {handleInputChange} />
             
+            <div classname= 'mt-3'>
+                <div className='table'>
+                    <div className='cell'>
+                            <h2 className="table-heading" style={styles.cell}>First Name</h2>
+                    </div>
+                    <div className='cell'>
+                            <h2 className="table-heading" style={styles.cell}>Last Name</h2>
+                    </div>
+                    <div className='cell'>
+                            <h2 className="table-heading" style={styles.cell}>Company</h2>
+                    </div>
+                    <div className='cell'>
+                            <h2 className="table-heading" style={styles.cell}>Email</h2>
+                    </div>
+                    <div className='cell'>
+                            <h2 className="table-heading" style={styles.cell}>Delete</h2>
+                    </div>
+                    
+
+                </div>
+            </div>
+           
+            <Footer />
         </div>
     )
 }
