637a4038b424ad9e601220e73f56f53497c0e686
Hayden Garrard <hagarrard@gmail.com>
2021-03-18 12:38:05 -0500

    cleaned up code on stats page

--- client/src/pages/Stats.js
+++ client/src/pages/Stats.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect }  from "react";
+import React, { useState, useEffect } from "react";
 import useAuth from '../utils/useAuth';
 import Navbar from '../components/Navbar';
 import Header from '../components/Header';
@@ -10,8 +10,8 @@ var tf = require('@tensorflow/tfjs');
 
 
 const Stats = () => {
-    
-useAuth();
+
+    useAuth();
     const [resultData, setResultData] = useState('')
 
     const [userState, setUserState] = useState({
@@ -20,8 +20,8 @@ useAuth();
         monthlyEarnings: '',
         predictionMonth: ''
     })
-    useEffect(()=>{
-    },[resultData])
+    useEffect(() => {
+    }, [resultData])
     const handleChange = e => {
         const value = e.target.value;
         const name = e.target.name;
@@ -41,11 +41,11 @@ useAuth();
 
         setUserState({ ...userState, [name]: value })
 
-      
+
 
         const { expensesVal, monthlyEarnings, predictionMonth } = userState
         var testmon = monthlyEarnings.split(',').map(x => +x)
-        
+
 
         predictor(expensesVal, monthlyEarnings, predictionMonth);
     }
@@ -58,16 +58,14 @@ useAuth();
         // creates a new array ignoring all the commas so i can count the array length
         const xUnitVal = [];
         // on a graph this is the variable where the x units are contained
-        
+
         xUnitsCount(xUnitsCountArr.length + 1);
         // 5 values inputted then get 6, etc
-
-        
 
         function xUnitsCount(limit) {
             // this function counts for us so we can get the length the user inputted correctly and don't have to worry about any x value limiting.
             for (var i = 0; i < limit; i++) {
-               
+
 
                 xUnitVal.push(i);
             }
@@ -85,52 +83,30 @@ useAuth();
         model.add(tf.layers.dense({ units: 1, inputShape: [1] }));
 
         model.compile({ loss: 'meanSquaredError', optimizer: 'sgd', metrics: ['accuracy'] })
-        
 
         const yUnitVal = revenue
         yUnitVal.unshift(-expensesVal)
 
-        
-
         const xs = tf.tensor2d(xUnitVal, [xUnitVal.length, 1])
         const ys = tf.tensor2d(yUnitVal, [xUnitVal.length, 1])
         if (predictionMonth < xUnitVal.length + 1) { alert("please make sure you are asking to predict in the future!, make sure to add one month") }
-        else { 
+        else {
             model.fit(xs, ys, { epochs: 100 }).then(() => {
-                  model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).print();
+                model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).print();
                 //  console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth-xUnitVal)], [1,1])))
                 let resultTest = [];
                 // console.log(resultTest);
                 // gave me an error here so i ended up having to parsefloat
                 for (let index = 14; index < 24; index++) {
                     resultTest.push(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[index])
-                    
+
                 }
-                
-                setResultData(resultTest); 
-                
 
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[14])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[15])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[16])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[17])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[18])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[19])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[20])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[21])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[22])
-                // console.log(model.predict(tf.tensor2d([parseFloat(predictionMonth)], [1, 1])).toString()[23])
-                
-
-                
-               
-
-                
+                setResultData(resultTest);
 
             });
         }
     }
-    
 
     return (
         //   wrap body tag here so the page looks all pretty
@@ -138,11 +114,11 @@ useAuth();
         <div>
             <Navbar />
             <Header />
-            <form 
-            className="control" onSubmit={handleSubmit}>
+            <form
+                className="control" onSubmit={handleSubmit}>
                 <Input
                     type="text"
-                    
+
                     value={userState.expensesVal}
                     name="expensesVal"
                     handleChange={handleChange}
@@ -164,12 +140,12 @@ useAuth();
                     placeholder="How many months in the future would you like me to predict?"
                 />
                 <Button
-                name="Predict for me!"
-                type="submit"
-                color="#fb8500" />
+                    name="Predict for me!"
+                    type="submit"
+                    color="#fb8500" />
 
                 <p>your value for your monthly earnings is <p>{resultData}</p>
-                    
+
                 </p>
             </form>
             <Footer />
